c4da83cce96f1cdc4afaf90b7d6e7d2833723c00
Nestor <nestorchaoberger@gmail.com>
2024-01-18 12:33:49 +0100

    Added Card visible on isExpanded in Recipe Card

--- app/src/main/java/com/example/deliciousandy/components/RecipeCard.kt
+++ app/src/main/java/com/example/deliciousandy/components/RecipeCard.kt
@@ -1,8 +1,12 @@
 package com.example.deliciousandy.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -11,6 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.deliciousandy.data.Recipe
@@ -33,6 +38,37 @@ fun RecipeCard(recipe: Recipe) {
         ) {
             Text(text = recipe.name)
         }
+        if (isExpanded) {
+            Card(
+                modifier = Modifier
+                    .fillMaxSize() // Make the Card take the full available space
+                    .clickable {
+                        isExpanded = !isExpanded
+                    }
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize() // Make the Column take the full available space
+                        .padding(16.dp), // Add padding for better aesthetics
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = recipe.name,
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier
+                            .fillMaxWidth() // Make the Text take the full width
+                            .padding(bottom = 8.dp) // Add bottom padding for separation
+                    )
+                    Text(
+                        text = recipe.body,
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.fillMaxWidth() // Make the Text take the full width
+                    )
+                }
+            }
 
+        }
     }
+
 }
