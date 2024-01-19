81723331560e246deb9655fd0b32aa62e8dc9c7f
Nestor Chao Berger <103984186+NeonBurst@users.noreply.github.com>
2024-01-19 13:02:27 +0100

    Remove Message and Preview functions (#12)
    Co-authored-by: Nestor <nestorchaoberger@gmail.com>

--- app/src/main/kotlin/com/example/deliciousandy/MainActivity.kt
+++ app/src/main/kotlin/com/example/deliciousandy/MainActivity.kt
@@ -190,19 +190,3 @@ class MainActivity : ComponentActivity() {
     }
 
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DeliciousAndyTheme {
-        Greeting("Android")
-    }
-}
