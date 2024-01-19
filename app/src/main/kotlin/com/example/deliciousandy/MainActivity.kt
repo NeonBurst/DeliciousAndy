b481d1788d217af630e07b52d928e77bb16de870
Nestor Chao Berger <103984186+NeonBurst@users.noreply.github.com>
2024-01-19 11:25:15 +0100

    Housekeeping/introduce scaffolding (#7)
    * Scaffolding WIP
    
    * Added Scaffold
    
    ---------
    
    Co-authored-by: Nestor <nestorchaoberger@gmail.com>

--- app/src/main/kotlin/com/example/deliciousandy/MainActivity.kt
+++ app/src/main/kotlin/com/example/deliciousandy/MainActivity.kt
@@ -19,7 +19,7 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -43,7 +43,7 @@ import com.example.deliciousandy.utility.ConverterJSON
 // Add edit recipe
 
 class MainActivity : ComponentActivity() {
-    
+
     private var recipeList: List<Recipe> = listOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,20 +53,26 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun update() {
-
         setContent {
             DeliciousAndyTheme {
+                var showAddUI: Boolean by remember { mutableStateOf(false) }
+
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    var showAddUI: Boolean by remember { mutableStateOf(false) }
-                    Column {
-                        Text(" Recipes", style = MaterialTheme.typography.titleLarge)
-                        FloatingActionButton(onClick = {showAddUI = !showAddUI}) {
+                Scaffold(
+
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = { showAddUI = !showAddUI }) {
                             Icon(Icons.Filled.Add, "Add button")
                         }
+                    },
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = MaterialTheme.colorScheme.background
+
+                ) { padding ->
+
+                    Column {
+                        Text(" Recipes", style = MaterialTheme.typography.titleLarge)
+
                         LazyColumn {
                             items(recipeList) { sample ->
                                 RecipeCard(sample)
@@ -75,59 +81,64 @@ class MainActivity : ComponentActivity() {
                         }
 
                     }
-                    if (showAddUI) {
-                        Card(modifier = Modifier
-                            .size(width = 240.dp, height = 100.dp)) {
-                            Column {
-                                Text(
-                                    text = "Add Recipe",
-                                    modifier = Modifier
-                                        .padding(16.dp),
-                                    textAlign = TextAlign.Center,
+
+
+
+                }
+
+                if (showAddUI) {
+                    Card(
+                        modifier = Modifier
+                        .fillMaxSize()) {
+                        Column {
+                            Text(
+                                text = "Add Recipe",
+                                modifier = Modifier
+                                    .padding(16.dp),
+                                textAlign = TextAlign.Center,
+                            )
+
+                            var titleText by remember { mutableStateOf("") }
+                            TextField(
+                                value = titleText,
+                                onValueChange = { newText ->
+                                    titleText = newText
+                                },
+                                label = { Text(text = "Recipe Name") },
+                                maxLines = 1
+
+                            )
+
+                            Divider(modifier = Modifier.size(20.dp), color = Color.Transparent)
+
+                            var bodyText by remember { mutableStateOf("") }
+                            TextField(
+                                value = bodyText,
+                                onValueChange = { newText ->
+                                    bodyText = newText
+                                },
+                                label = { Text(text = "Recipe Instructions") },
+
                                 )
 
-                                var titleText by remember { mutableStateOf("") }
-                                TextField(
-                                    value = titleText,
-                                    onValueChange = { newText ->
-                                        titleText = newText
-                                    },
-                                    label = { Text(text = "Recipe Name") },
-                                    maxLines = 1
+                            ActivityResultContracts.PickVisualMedia()
 
-                                )
-
-                                Divider(modifier = Modifier.size(20.dp), color = Color.Transparent)
-
-                                var bodyText by remember { mutableStateOf("") }
-                                TextField(
-                                    value = bodyText,
-                                    onValueChange = { newText ->
-                                        bodyText = newText
-                                    },
-                                    label = { Text(text = "Recipe Instructions") },
-
-                                )
-
-                                ActivityResultContracts.PickVisualMedia()
-
-                                Button(onClick = {
-                                    addRecipe(Recipe(titleText, bodyText))
-                                    showAddUI = false
-                                }) {
-                                    Text("Add")
-                                }
+                            Button(onClick = {
+                                addRecipe(Recipe(titleText, bodyText))
+                                showAddUI = false
+                            }) {
+                                Text("Add")
                             }
-
-
-
                         }
+
+
                     }
                 }
+
             }
         }
     }
-    
+
     private fun addRecipe(recipe: Recipe) {
         recipeList += recipe
         /*val j = ConverterJSON()
@@ -136,7 +147,11 @@ class MainActivity : ComponentActivity() {
         println("Hi Jason" + j.loadJsonFromFile("recipe.json", this))*/
 
         val converterJSON = ConverterJSON()
-        converterJSON.saveJsonToFile(converterJSON.convertRecipeJSON(AppData("U", recipeList)), "data.json", this)
+        converterJSON.saveJsonToFile(
+            converterJSON.convertRecipeJSON(AppData("U", recipeList)),
+            "data.json",
+            this
+        )
         update()
     }
 
