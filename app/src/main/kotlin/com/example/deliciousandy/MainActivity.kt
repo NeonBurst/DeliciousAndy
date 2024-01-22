5ea48159e75ed662c23c23d591a83621e2c82b0f
Nestor <nestorchaoberger@gmail.com>
2024-01-22 10:52:03 +0100

    Added text field for serving size

--- app/src/main/kotlin/com/example/deliciousandy/MainActivity.kt
+++ app/src/main/kotlin/com/example/deliciousandy/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
@@ -32,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.deliciousandy.components.RecipeCard
@@ -123,7 +124,18 @@ class MainActivity : ComponentActivity() {
 
                                 )
 
-                            ActivityResultContracts.PickVisualMedia()
+                            var servingSize by remember { mutableStateOf("") }
+                            TextField(
+                                value = servingSize,
+                                onValueChange = { newText ->
+                                    servingSize = newText
+                                },
+                                label = { Text(text = "Serving Size") },
+                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+
+                                )
+
+
 
                             Row {
                                 Button(
