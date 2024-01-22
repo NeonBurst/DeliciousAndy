d390515e188b5bdeda133f98478b08a0922413fc
Nestor <nestorchaoberger@gmail.com>
2024-01-22 13:23:02 +0100

    Added bottom bar

--- app/src/main/kotlin/com/example/deliciousandy/MainActivity.kt
+++ app/src/main/kotlin/com/example/deliciousandy/MainActivity.kt
@@ -14,6 +14,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -21,6 +25,7 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -32,7 +37,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -69,6 +73,22 @@ class MainActivity : ComponentActivity() {
                         FloatingActionButton(onClick = { showAddUI = !showAddUI }) {
                             Icon(Icons.Filled.Add, "Add button")
                         }
+
+
+                    },
+                    bottomBar = { BottomAppBar(
+                        actions = {
+                        IconButton(onClick = { /* do something */ }) {
+                            Icon(Icons.Filled.Home, contentDescription = "Localized description")
+                        }
+                        IconButton(onClick = { /* do something */ }) {
+                            Icon(Icons.Filled.Info, contentDescription = "Localized description")
+                        }
+                        IconButton(onClick = { /* do something */ }) {
+                                Icon(Icons.Filled.Settings, contentDescription = "Localized description")
+                        }
+                      }
+                    )
                     },
                     modifier = Modifier.fillMaxSize(),
                     containerColor = MaterialTheme.colorScheme.background
@@ -78,7 +98,7 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier
                             .padding(padding)
                     ) {
-                        if (recipeList.size > 0) {
+                        if (recipeList.isNotEmpty()) {
                             LazyColumn {
                                 items(recipeList) { sample ->
                                     RecipeCard(sample)
