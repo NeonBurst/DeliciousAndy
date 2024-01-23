a83518ce92d490dedaec5461a33a8296416c679c
Nestor <nestorchaoberger@gmail.com>
2024-01-23 10:34:51 +0100

    Added remaining Strings

--- app/src/main/kotlin/com/example/deliciousandy/ui/components/RecipeCard.kt
+++ app/src/main/kotlin/com/example/deliciousandy/ui/components/RecipeCard.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.deliciousandy.R
@@ -113,20 +114,20 @@ fun RecipeCard(
                         IconBtn(
                             imageVector = Icons.Default.Star,
                             tint = if (recipe.starred) Color.Yellow else Color.Black,
-                            contentDescription = "",
+                            contentDescription = stringResource(R.string.favourite),
                         ) { println("Star Pressed") }
 
                         IconBtn(
                             imageVector = Icons.Default.Edit,
                             tint = Color.Black,
-                            contentDescription = "",
+                            contentDescription = stringResource(R.string.edit),
                         ) { println("Edit Pressed") }
 
 
                         IconBtn(
                             imageVector = Icons.Default.Delete,
                             tint = Color.Black,
-                            contentDescription = "",
+                            contentDescription = stringResource(R.string.delete),
                         ) { println("Delete Pressed") }
 
                     }
