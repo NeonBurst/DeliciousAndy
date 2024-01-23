e4864a6a75b49f10ba8085e87169d7df2952ec42
Nestor <nestorchaoberger@gmail.com>
2024-01-23 10:21:18 +0100

    Added Comments

--- app/src/main/kotlin/com/example/deliciousandy/ui/theme/Color.kt
+++ app/src/main/kotlin/com/example/deliciousandy/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.example.deliciousandy.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+val Purple80 = Color(0xFFD0BCFF) // TODO  Standardize Colors
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
