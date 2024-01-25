748ceabd923a98a4e6d2b63a9c22f96d7f378d4d
NeonBurst <nestorchaoberger@icloud.com>
2024-01-25 10:58:25 +0100

    ViewModel added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,17 +51,26 @@ android {
 
 dependencies {
 
+    // Android
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+
+    // Compose
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.navigation:navigation-compose:2.7.6")
+
+    // Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // Testing
     testImplementation("junit:junit:4.13.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
